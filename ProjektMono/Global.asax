2d6c7c33a0d53e354c1beab722d198bf3bac1299
iPaich <ipaic47@gmail.com>
2018-08-14 20:06:29 +0200

    Add project files.

---
+++ ProjektMono/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjektMono.MvcApplication" Language="C#" %>
---
+++ ProjektMono/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjektMono.Startup))]
+namespace ProjektMono
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
